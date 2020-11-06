22b766230946546d573b810403a88343ce97ee73
Kristen Collier <kristencollier10@gmail.com>
2020-11-06 17:00:16 -0600

    updates

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
-    html += '<br>' + coffee.id + '<br>';
-    html += '<br>' + coffee.name + '<br>';
-    html += '<br>' + coffee.roast + '<br>';
+    let html = '<p class="coffee">';
+    // html += '<br>' + coffee.id + '<br>';
+    html += '<br>' + coffee.name + ' ';
+    html += coffee.roast + '<br>';
     // html += '</tr>';
 
     return html;
 }
+// prints out coffees
 
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    let html = '';
+    for(let i = coffees.length - 1; i >= 0; i--) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
+// turns each coffee into a string and displays
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
+    let selectedRoast = roastSelection.value;
+    let filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    covfefe.innerHTML = renderCoffees(filteredCoffees);
 }
+// form for the light, medium, dark
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
+let coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
@@ -47,11 +50,23 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
+// array of coffees
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+let covfefe = document.querySelector('#coffees');
+let submitButton = document.querySelector('#submit');
+let roastSelection = document.querySelector('#roast-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
+
+covfefe.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+
+document.getElementById('coffeeName').style.width="100%";
+document.getElementById('coffeeName').style.fontSize="14pt";
+document.getElementById('roast-selection').style.width="100%";
+document.getElementById('roast-selection').style.fontSize="14pt";
+document.getElementById('submit').style.width="100%";
+document.getElementById('submit').style.fontSize="14pt";
+document.getElementById('submit').style.backgroundColor="#00ccdd";
+
+
